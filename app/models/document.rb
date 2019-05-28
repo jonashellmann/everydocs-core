3379d2887c8404c7d3d66b95cc0e2afa7563de7a
Gogs <gogs@fake.local>
2019-05-28 13:32:59 +0200

    Change JSON representation of documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,20 @@ class Document < ActiveRecord::Base
   belongs_to :folder
   belongs_to :user
   belongs_to :state
+  belongs_to :person
 
   has_many :documenttags
-  has_many :documents, through: :documenttags
+  has_many :tags, through: :documenttags
 
   validates_presence_of :title, :document_date, :document_url, :user, :state, :folder
+
+  def as_json(_options = {})
+    super include: {
+      folder: {only: [:id, :name]},
+      state: {only: [:id, :name]},
+      tags: {only: [:id, :name]},
+      user: {only: [:id, :name]},
+      person: {only: [:id, :name]}, 
+    }
+  end
 end
