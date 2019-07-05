443686f0f2e5ae12259540c353727e63204dfd30
Gogs <gogs@fake.local>
2019-07-05 10:03:13 +0200

    Catch error when user mail already exists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
     json_response(response, :created)
+
+    rescue StandardError => e
+      json_response({message: 'There is already an account with this email!'})
   end
 
   private
