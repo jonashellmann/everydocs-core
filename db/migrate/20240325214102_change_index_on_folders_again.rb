4fe78fb7ff906c5c92d28f217114332a23dbce7a
Jonas Hellmann <hellmann.jonas@web.de>
2024-03-25 21:55:42 +0100

    Fix name of db migration

--- db/migrate/20240325214102_change_index_on_folders_again.rb
+++ db/migrate/20240325214102_change_index_on_folders_again.rb
@@ -1,4 +1,4 @@
-class ChangeIndexOnFolders < ActiveRecord::Migration[7.0]
+class ChangeIndexOnFoldersAgain < ActiveRecord::Migration[7.0]
   def change
     remove_index :folders, name: "index_folders_on_name_and_user_id"
     add_index :folders, [:name, :user_id, :folder_id], unique: true
